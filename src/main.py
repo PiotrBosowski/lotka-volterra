066c052b35172671f23cf3860a3b34974744414f
Piotr Bosowski <piotr.bosowski@gmail.com>
2025-05-24 23:56:06 +0200

    fix: make sidebar compact

--- src/main.py
+++ src/main.py
@@ -15,11 +15,10 @@ models = {
     "Prey shelters": LotkaVolteraPreyShelters,
 }
 
-st.sidebar.header("Model Selection")
 model_choice = st.sidebar.selectbox("Choose a model:", models.keys())
 
 # Shared sliders (used in all models)
-st.sidebar.header("Common parameters")
+st.sidebar.markdown("**Common parameters**")
 r = st.sidebar.slider("r - Prey's (r)eproduction rate", 0.1, 1.0, 0.6, step=0.01)
 a = st.sidebar.slider("a - Predation rate", 0.01, 1.0, 0.4, step=0.01)
 b = st.sidebar.slider("b - Predation-reproducing rate", 0.01, 1.0, 0.1, step=0.01)
@@ -38,27 +37,27 @@ elif model_choice == "Prey shelters":
     s = st.sidebar.slider("s - Number of shelters", 0, 25, 3, step=1)
     model_params["s"] = s
 
-st.sidebar.header("Initial Populations")
+st.sidebar.markdown("**Initial populations**")
 V0 = st.sidebar.slider("Initial Prey (V₀)", 1, 50, 10, step=1)
 P0 = st.sidebar.slider("Initial Predator (P₀)", 1, 50, 5, step=1)
 
-st.sidebar.header("Simulation Settings")
-T = st.sidebar.slider("Simulation Duration (T)", 10, 200, 50, step=1)
-
+with st.sidebar.expander("Simulation settings"):
+    T = st.slider("Simulation Duration (T)", 10, 200, 50, step=1)
+    resolution = st.slider("Time resolution (points per unit)", 1, 100, 5, step=1)
+    solver_choice = st.selectbox('Choose a solver:', ['DOP853', 'RK45', 'RK23'])
 
 model = models[model_choice](**model_params)
 
 
-def solve_ode(model, time_limit):
+def solve_ode(model, time_limit, solver, resolution=10):
     t_span = (0, time_limit)
-    ticks_per_unit = 10
-    t_eval = np.linspace(t_span[0], t_span[1], time_limit * ticks_per_unit)
-    sol = solve_ivp(fun=model, t_span=t_span, y0=[V0, P0], method='DOP853', t_eval=t_eval)
+    t_eval = np.linspace(t_span[0], t_span[1], time_limit * resolution)
+    sol = solve_ivp(fun=model, t_span=t_span, y0=[V0, P0], method=solver, t_eval=t_eval)
     t, prey, predator = sol.t, sol.y[0], sol.y[1]
     return t, prey, predator
 
 
-t, V, P = solve_ode(model=model, time_limit=T)
+t, V, P = solve_ode(model=model, solver=solver_choice, time_limit=T, resolution=resolution)
 
 def plot_series_and_phase(t, V, P):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
