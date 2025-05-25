0db6536ede005858541fba79d8d37a388beb2ada
Piotr Bosowski <piotr.bosowski@gmail.com>
2025-05-25 21:41:18 +0200

    fix: equations

--- src/models.py
+++ src/models.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class LotkaVoltera:
     def __init__(self, r: float, a: float, b: float, m: float):
         self.r = r
@@ -11,8 +14,11 @@ class LotkaVoltera:
         dPdt = - self.m * P + self.b * V * P 
         return [dVdt, dPdt]
     
+    def stability_points(self) -> List:
+        return [(0, 0), (self.r / self.a, self.m / (self.a * self.b))]
 
-class LotkaVolteraLimitedEnviron:
+
+class LotkaVolteraLimitedEnviron(LotkaVoltera):
     def __init__(self, r: float, a: float, b: float, m: float, k: float):
         self.r = r
         self.a = a
@@ -23,11 +29,11 @@ class LotkaVolteraLimitedEnviron:
     def __call__(self, _, current_point):
         V, P = current_point
         dVdt = self.r * V * (1 - V / self.k) - self.a * V * P
-        dPdt = - self.m * P + self.b * V * P 
+        dPdt = - self.m * P + self.a * self.b * V * P 
         return [dVdt, dPdt]
     
 
-class LotkaVolteraPreyShelters:
+class LotkaVolteraPreyShelters(LotkaVoltera):
     def __init__(self, r: float, a: float, b: float, m: float, s: float):
         self.r = r
         self.a = a
